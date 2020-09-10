6a76792293f0667399800043c53c50db1cbb9425
Efrat Portal <eportal@infinidat.com>
2020-09-10 16:46:38 +0300

    remove unused css

--- pages/api/api.js
+++ pages/api/api.js
@@ -4,7 +4,6 @@ import json from './sidebar.json'
 export const getData = async (url = URL) => {
     try {
         /* const response = await axios.get(url)  ---> option to use real server url data in the future */
-        /*   return sorted data by name */
         return json
     }
     catch (error) {
